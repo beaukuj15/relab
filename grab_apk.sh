318b5d3d522fe5d3d757e6bc6f191f540b8429f4
Beau Kujath <beau@breakpointingbad.com>
2023-04-03 18:45:28 -0600

    adding prereq install script

--- grab_apk.sh
+++ grab_apk.sh
@@ -13,19 +13,12 @@ mkdir -p ./apks
 printf "Downloading bundle id: $BUNDLE_ID\n\n"
 
 APK=$(adb shell pm path $BUNDLE_ID)
-
 printf "\nAPK path on device: $APK\n"
 
-
 APK=`echo $APK | awk '{print $NF}' FS=':' | tr -d '\r\n'`
-
 printf "\napk file:  $APK\n"
 
 NEW_APK_FP="./apks/$BUNDLE_ID.apk"
 
-
-
 adb pull $APK $NEW_APK_FP
-
-
 printf "\nDownloaded apk file: $NEW_APK_FP\n"
---
+++ setup_scripts/install_prereqs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+
+
+
+printf "Installing prereqs including python3, pip, dev-tools, and apktool..\n"
+
+
+sudo apt install python3 -y
+
+sudo apt install python3-pip -y
+
+sudo apt install build-essential -y
+
+
+sudo apt install apktool -y
